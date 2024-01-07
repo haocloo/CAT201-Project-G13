ea8cbf0a8bfe3d14017309feb0901225e465e4c4
Chi Hao <81066169+haocloo@users.noreply.github.com>
2024-01-07 15:48:25 +0800

    Testing switching scene

--- app/src/main/java/cat201/project/controllers/LoginController.java
+++ app/src/main/java/cat201/project/controllers/LoginController.java
@@ -1,29 +1,26 @@
 package cat201.project.controllers;
 
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 
 public class LoginController {
 
+    // Change scene
     @FXML
-    private ImageView dish1ImageView;
-    @FXML
-    private ImageView dish2ImageView;
-    @FXML
-    private ImageView dish3ImageView;
-    @FXML
-    private ImageView dish4ImageView;
-    @FXML
-    private ImageView dish5ImageView;
-
-    @FXML
-    public void initialize() {
-        dish1ImageView.setImage(new Image("/images/dish1.png"));
-        dish2ImageView.setImage(new Image("/images/dish2.png"));
-        dish3ImageView.setImage(new Image("/images/dish3.png"));
-        dish4ImageView.setImage(new Image("/images/dish4.png"));
-        dish5ImageView.setImage(new Image("/images/dish5.png"));
+    public void handleLoginButtonAction(ActionEvent event) throws IOException {
+        URL url = getClass().getClassLoader().getResource("views/Main.fxml");
+        Parent root = FXMLLoader.load(url);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
