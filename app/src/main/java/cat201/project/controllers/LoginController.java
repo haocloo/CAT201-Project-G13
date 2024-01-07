b2d6e8e897c8de56f1b375d5401c9faf5db51f79
Chi Hao <81066169+haocloo@users.noreply.github.com>
2024-01-07 16:10:59 +0800

    Test pass data between controllers of different scenes

--- app/src/main/java/cat201/project/controllers/LoginController.java
+++ app/src/main/java/cat201/project/controllers/LoginController.java
@@ -1,23 +1,32 @@
 package cat201.project.controllers;
 
 import java.io.IOException;
-import java.net.URL;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 public class LoginController {
 
+    @FXML
+    TextField nameTextField;
+
     // Change scene
     @FXML
-    public void handleLoginButtonAction(ActionEvent event) throws IOException {
-        URL url = getClass().getClassLoader().getResource("views/Main.fxml");
-        Parent root = FXMLLoader.load(url);
+    public void login(ActionEvent event) throws IOException {
+        String username = nameTextField.getText();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("views/Main.fxml"));
+        Parent root = loader.load();
+        
+        MainController mainController = loader.getController();
+        mainController.displayName(username);
+        
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
--- app/src/main/java/cat201/project/controllers/MainController.java
+++ app/src/main/java/cat201/project/controllers/MainController.java
@@ -2,6 +2,7 @@ package cat201.project.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.shape.Circle;
 
 public class MainController {
@@ -10,6 +11,13 @@ public class MainController {
     private Circle myCircle;
     private double x;
     private double y;
+
+    @FXML
+    Label nameLabel;
+
+    public void displayName(String name) {
+        nameLabel.setText("Hello, " + name + "!");
+    }
 
     public void up(ActionEvent event) {
         myCircle.setCenterY(y -= 10);
