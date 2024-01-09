b0040100b16262dc398df301dc7f2afb7b7d6a9d
Chi Hao <81066169+haocloo@users.noreply.github.com>
2024-01-10 01:11:41 +0800

    Add customer and receipt tables to setup.sql

--- app/src/main/java/cat201/project/model/setup.sql
+++ app/src/main/java/cat201/project/model/setup.sql
@@ -15,9 +15,27 @@ CREATE TABLE IF NOT EXISTS product (
     prod_id VARCHAR(100),
     prod_name VARCHAR(100),
     stock INT(100),
-    type VARCHAR(100) ,
+    type VARCHAR(100),
     price DOUBLE,
     status VARCHAR(100),
     image VARCHAR(500),
     date DATE DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS customer (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT(100),
+    prod_name VARCHAR(100),
+    quantity INT(100),
+    price DOUBLE,
+    date DATE DEFAULT NULL,
+    em_username VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS receipt (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT(100),
+    total DOUBLE,
+    date DATE DEFAULT NULL,
+    em_username VARCHAR(100)
 );
